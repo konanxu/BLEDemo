0f7530aeb29fda27158e5baea8441c0737dd173d
konanxu <konanxuxi@gmail.com>
2016-04-14 16:15:25 +0800

    init

---
+++ BLEDemo/CenterBLEViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterBLEViewController.h
+//  BLEDemo
+//
+//  Created by Konan on 16/4/13.
+//  Copyright © 2016年 Konan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterBLEViewController : UIViewController
+
+@end
---
+++ BLEDemo/PeripheralBLEViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeripheralBLEViewController.h
+//  BLEDemo
+//
+//  Created by Konan on 16/4/13.
+//  Copyright © 2016年 Konan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeripheralBLEViewController : UIViewController
+
+@end
